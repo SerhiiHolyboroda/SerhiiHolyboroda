6b1a2bdc380bdc75d18001df7d00a2107e6e7a41
Serhii Holyboroda <49484600+SerhiiHolyboroda@users.noreply.github.com>
2021-03-01 15:06:21 +0200

    added more bags

--- src/main/java/com.io.json/classes/DataJson.java
+++ src/main/java/com.io.json/classes/DataJson.java
@@ -5,4 +5,40 @@ public class DataJson {
     private String positionText;
     private String points;
     private String wins;
+
+    public DataJson() {
+    }
+
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getPositionText() {
+        return positionText;
+    }
+
+    public void setPositionText(String positionText) {
+        this.positionText = positionText;
+    }
+
+    public String getPoints() {
+        return points;
+    }
+
+    public void setPoints(String points) {
+        this.points = points;
+    }
+
+    public String getWins() {
+        return wins;
+    }
+
+    public void setWins(String wins) {
+        this.wins = wins;
+    }
 }
